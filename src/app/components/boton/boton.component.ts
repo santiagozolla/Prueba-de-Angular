70d951e755e7cd2107e1c5b7540ebf34c6db597d
Santiago Zolla <santiago.zolla.97@gmail.com>
2022-03-24 19:51:45 -0300

    Primer Boton

---
+++ src/app/components/boton/boton.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-boton',
+  templateUrl: './boton.component.html',
+  styleUrls: ['./boton.component.css']
+})
+export class BotonComponent implements OnInit {
+  @Input() text:string = "";
+  @Input() color:string = "";
+  @Output() btnClick = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(){
+    console.log("Click");
+  }
+
+}
